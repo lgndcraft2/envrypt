ae6205ceee977971a302535a9a77475b452333a8
Raheem Akapo <akapsyjr@gmail.com>
2026-01-27 08:47:28 +0100

    feat: remove unused Navbar import from WaitlistPage component causing build error

--- frontend/src/components/WaitlistPage.tsx
+++ frontend/src/components/WaitlistPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { api } from '../lib/api';
-import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
 
 const WaitlistPage: React.FC = () => {
